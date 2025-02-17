883e1ed352f387d44fce60f83e4a2082972e8d09
Jp <juanpereira.liza@gmail.com>
2025-02-17 11:44:45 -0500

    commit axios

---
+++ src/plugins/axios.js
@@ -0,0 +1,20 @@
+import Axios from "axios";
+
+const token = JSON.parse(localStorage.getItem("auth"));
+
+if (token) {
+    console.log("token encontrado", token); 
+} else{
+    console.log("token no encontrado");
+}
+
+console.log("mitoken", token);
+
+const apiClient = Axios.create({
+    baseURL: 'https://api-sandbox.factus.com.co/',
+    headers: {
+        "x-token": token ? token.token : "",
+    }
+});
+
+export default apiClient;
