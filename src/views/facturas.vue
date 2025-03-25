2654f63f1c145c19689e2829e9793f3b74ff8e26
juan <juanpereira.liza@gmail.com>
2025-03-25 05:23:44 -0500

    first facturas

--- src/views/facturas.vue
+++ src/views/facturas.vue
@@ -16,51 +16,121 @@
         </q-header>
         <q-page-container>
             <div class="q-pa-md q-gutter-sm">
-                <q-btn v-for="filter in backdropFilterList" :key="filter.label" color="primary" :label="filter.label"
-                    no-caps @click="filter.onClick" />
+                <q-btn label="Maximized" color="primary" @click="dialog = true" />
 
-                <q-dialog v-model="dialog" :backdrop-filter="backdropFilter">
-                    <q-card>
-                        <q-card-section class="row items-center q-pb-none text-h6">
-                            Dialog
-                        </q-card-section>
+                <q-dialog v-model="dialog" persistent :maximized="maximizedToggle" transition-show="slide-up"
+                    transition-hide="slide-down">
+                    <q-card class="bg-primary text-white">
+                        <q-bar>
+                            <q-space />
+                            <q-btn dense flat icon="close" v-close-popup>
+                                <q-tooltip class="bg-white text-primary">Close</q-tooltip>
+                            </q-btn>
+                        </q-bar>
 
                         <q-card-section>
-                            This dialog has a backdrop filter of {{ backdropFilter }}.
+                            <div class="text-h6">Crear Factura</div>
                         </q-card-section>
 
-                        <q-card-actions align="right">
-                            <q-btn flat label="Close" color="primary" v-close-popup />
-                        </q-card-actions>
+                        <q-card-section class="q-pt-none">
+                            <h5 style="text-align: center;">Cliente</h5>
+                            <div class="cliente">
+                                <div>
+                                    <q-input outlined v-model="typeOfPerson" label="Tipo de cliente" filled
+                                        class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El tipo de cliente es obligatorio']" />
+
+                                    <q-input outlined v-model="typeOfIdentification" label="Tipo de identificacion"
+                                        filled class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El tipo de identificacion es obligatorio']" />
+
+                                    <q-input outlined v-model="identification" label="numero de identificacion" filled
+                                        class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+
+                                    <q-input outlined v-model="names" label="Nombre" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+
+                                </div>
+
+                                <div>
+                                    <q-input outlined v-model="names" label="Nombre" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+
+                                    <q-input outlined v-model="addres" label="Direccion" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+
+                                    <q-input outlined v-model="email" label="Correo electronico" filled
+                                        class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+
+                                    <q-input outlined v-model="phone" label="Telefono" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El numero de identificacion es obligatorio']" />
+                                </div>
+
+                                <div>
+                                    <q-input outlined v-model="dv" label="dv" filled class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                    <q-input outlined v-model="company" label="Compañia" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                    <q-input outlined v-model="tradeName" label="Trade Name" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                    <q-input outlined v-model="tradeName" label="Trade Name" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                </div>
+
+                                <div>
+                                    <q-input outlined v-model="legalOrganizationId" label="Id de organizacion" filled
+                                        class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                    <q-input outlined v-model="tributeId" label="Tributo" filled class="custom-input"
+                                        type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+                                    <q-input outlined v-model="municipalityId" label="Municipio" filled
+                                        class="custom-input" type="String"
+                                        :rules="[val => val && val.length > 0 || 'El dv es obligatorio']" />
+
+
+                                </div>
+                            </div>
+                            <h5 style="text-align: center;">Productos</h5>
+                            <div class="Productos">
+
+                            </div>
+                        </q-card-section>
                     </q-card>
                 </q-dialog>
             </div>
+
             <div class="q-pa-md">
-                <q-table flat bordered :rows="rows" :columns="columns" row-key="name" />
+                <q-table style="height: 400px" flat bordered title="Treats" :rows="rows" :columns="columns"
+                    row-key="index" virtual-scroll v-model:pagination="pagination" :rows-per-page-options="[0]" />
             </div>
+
+
         </q-page-container>
+
     </q-layout>
+
 </template>
 <script setup>
 import { ref } from 'vue'
 
-// Lista de filtros para el fondo (backdrop-filter)
-const list = [
-    'Agregar factura',
-
-]
-
-const dialog = ref(false) // Controla la visibilidad del diálogo
-const backdropFilter = ref(null) // El filtro de fondo seleccionado
-
-// Genera la lista de filtros de fondo con una función de clic para aplicar el filtro
-const backdropFilterList = list.map(filter => ({
-    label: filter,
-    onClick: () => {
-        backdropFilter.value = filter
-        dialog.value = true
-    }
-}))
+const dialog = ref(false)
+const maximizedToggle = ref(true)
 
 const columns = [
     {
@@ -84,6 +154,7 @@ const columns = [
 ]
 
 const rows = ref([])
+
 
 </script>
 
@@ -122,5 +193,10 @@ const rows = ref([])
     display: flex;
     list-style: none;
 
+}
+
+.cliente {
+    display: flex;
+    justify-content: center;
 }
 </style>
